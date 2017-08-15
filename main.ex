070568f5bb52037a3a9a2f723db5ceda48743c94
Brian Hodges <bhodges@PMIDSD.com>
2017-08-15 16:45:23 -0400

    clean up struct

--- main.ex
+++ main.ex
@@ -1,12 +1,12 @@
 defmodule Country do
 	defstruct name: "", population: ""
 
-	def set_name(user, name) do
-	  %{user | name: name}
+	def set_name(c, name) do
+	  %{c | name: name}
 	end
 	
-	def set_population(user, population) do
-	  %{user | population: population}
+	def set_population(c, population) do
+	  %{c | population: population}
 	end
 end
 
